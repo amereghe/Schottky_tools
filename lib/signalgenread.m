37efc6fd0c3498fef78117a5433b9dd0d58a9b86
mauro-di-liddo <mauro.diliddo9@gmail.com>
2022-01-04 13:08:51 +0100

    funcion to read file .ini coming from function generator

---
+++ lib/signalgenread.m
@@ -0,0 +1,30 @@
+function [time,signal] = signalgenread()
+
+%OSCREAD used to save data coming from the genearor of functions in a .ini
+%file
+
+%   There are two sections in the file: the first is the header where we
+%   find the value of sampling frequency, gain of the acquisition chain and
+%   the repetition number (value) to ensure a continuous acquisition; the
+%   second section is storing the values of the generated signal.
+%   Temporal samples instants will be created by the sampling frequency and
+%   the length of the acquired array (signal).
+
+[file,folder]=uigetfile('*.ini');
+filename=fullfile(folder,file);
+
+ini=ini2struct(filename);
+fsamp=str2double(split(ini.header.fcamp,';'));
+signal=str2double(split(ini.signal.values,';'));
+
+fsamp=fsamp(1);
+if isnan(signal(end))
+    signal(end)=[];
+end
+
+deltaT=1/fsamp;
+time=(deltaT:deltaT:length(signal)*deltaT)'; %if we want to start from dt
+% time=(0:deltaT:(length(signal)-1)*deltaT)';
+
+end
+
