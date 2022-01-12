0c1ab8c01bc29b3db4b129b7103d63526bece001
mauro-di-liddo <mauro.diliddo9@gmail.com>
2022-01-12 10:41:52 +0100

    generation of sinusoid added

--- lib/mainosc.m
+++ lib/mainosc.m
@@ -9,8 +9,19 @@ fsamp=125*10^6; % Hz
 gain=1; % in range (1:6)
 repeat=100; % 0 -> continuous (need to stop); >0 -> single
 
+%% from main.m
+
 %setting desired signal
 signal=lu; % values taken from main.m
 
 %generation of the file .ini (need to add out.ini in your directory)
+signalgenwrite(filewrite,fileread,signal,fsamp,gain,repeat);
+
+%% section used to generate some sinusoids in order to evaluate pico's  and pick-up performances
+
+dt=1/fsamp; % temporal step
+intTime=200*10^-6;
+t=(0:dt:intTime-dt)'; % integration time for our sinusoid
+f=2*10^6; % Hz
+signal=sin(2*pi*f*t);
 signalgenwrite(filewrite,fileread,signal,fsamp,gain,repeat);
