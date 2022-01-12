f3eea38a887557b86e5f96ac695a103479e7b2fa
mauro-di-liddo <mauro.diliddo9@gmail.com>
2022-01-12 12:38:37 +0100

    gaussian pulse added to mainosc.m

--- lib/mainosc.m
+++ lib/mainosc.m
@@ -8,6 +8,8 @@ filewrite=fopen('out.ini','w');
 fsamp=125*10^6; % Hz
 gain=1; % in range (1:6)
 repeat=100; % 0 -> continuous (need to stop); >0 -> single
+dt=1/fsamp; % temporal step
+intTime=200*10^-6;
 
 %% from main.m
 
@@ -19,9 +21,15 @@ signalgenwrite(filewrite,fileread,signal,fsamp,gain,repeat);
 
 %% section used to generate some sinusoids in order to evaluate pico's  and pick-up performances
 
-dt=1/fsamp; % temporal step
-intTime=200*10^-6;
 t=(0:dt:intTime-dt)'; % integration time for our sinusoid
 f=2*10^6; % Hz
 signal=sin(2*pi*f*t);
+signalgenwrite(filewrite,fileread,signal,fsamp,gain,repeat);
+
+%% section used to generate gaussian pulse in order to evaluate pico's
+
+t=(-intTime/2:dt:intTime/2)';
+mu=0;
+sigma=0.00001;
+signal=exp(-(t-mu).^2/(2*sigma^2));
 signalgenwrite(filewrite,fileread,signal,fsamp,gain,repeat);
