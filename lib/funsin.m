f529b28895dd1529aa7d547b2feca2f5bc8d4511
mauro-di-liddo <mauro.diliddo9@gmail.com>
2022-01-18 11:19:11 +0100

    adapted to plot all curves from pico and Schottky

--- lib/funsin.m
+++ lib/funsin.m
@@ -1,30 +1,45 @@
-function [T,F,t,f] = funsin(file_fungen,file_pico,fsamp_pico,fsamp,time_matlab,freq_matlab,signal_matlab,sMATLAB)
+function [T,F,t,f] = funsin(file_fungen,file_pico,fsamp_pico,fsamp,time_matlab,freq_matlab,signal_matlab,sMATLAB,file_schottky)
 %MAINSIN Summary of this function goes here
 %   Detailed explanation goes here
 
 signal_fungen=funread(file_fungen);
 [time_pico,signal_pico]=oscread(file_pico);
+if exist(file_schottky,'file')
+    [time_schottky,signal_schottky]=oscread(file_schottky);
+end
 n_fungen=length(signal_fungen);
 n_pico=length(signal_pico);
-dt_fungen=1/fsamp;
 df_fungen=fsamp/n_fungen;
 df_pico=fsamp_pico/n_pico;
 
-time_fungen=(0:dt_fungen:n_fungen*dt_fungen)';
+time_fungen=time_matlab;
 freq_fungen=(0:df_fungen:fsamp-df_fungen)';
 freq_pico=(0:df_pico:fsamp_pico-df_pico)';
 
 T=padding(signal_matlab,signal_fungen);
 T=padding(T,signal_pico);
+if exist(file_schottky,'file')
+    T=padding(T,signal_schottky);
+end
 sFUNGEN=fft(signal_fungen);
 sPICO=fft(signal_pico);
 F=padding(sMATLAB,sFUNGEN);
 F=padding(F,sPICO);
+if exist(file_schottky,'file')
+    sSCHOTTKY=fft(signal_schottky);
+    F=padding(F,sSCHOTTKY);
+end
 
 t=padding(time_matlab,time_fungen);
 t=padding(t,time_pico);
+if exist(file_schottky,'file')
+    t=padding(t,time_schottky);
+end
 
 f=padding(freq_matlab,freq_fungen);
 f=padding(f,freq_pico);
+if exist(file_schottky,'file')
+    f=padding(f,freq_pico);
+end
 
 end
--- lib/mainosc.m
+++ lib/mainosc.m
@@ -1,17 +1,18 @@
 %% mainosc.m is used to take signals from main.m and use them as values in the .ini file
 
 %file manipulation
+
 fileread=fopen('template.ini','r');
 filewrite=fopen('out.ini','w');
 
 %% setting parameters
 
-fsamp=200*10^6; % Hz
+fsamp=125*10^6; % Hz --2bc
 gain=1; % in range (1:6)
 repeat=1; % 0 -> continuous (need to stop); >0 -> single
 dt=1/fsamp; % temporal step
 intTime=200*10^-6;
-fsamp_pico=125*10^6; %Hz
+fsamp_pico=125*10^6; %Hz --2bc
 
 %% from main.m
 
@@ -21,12 +22,15 @@ signal=lu; % values taken from main.m
 %generation of the file .ini (need to add out.ini in your directory)
 signalgenwrite(filewrite,fileread,signal,fsamp,gain,repeat);
 
+%% file_IDs for reading
+
+filename_fungen='20MHz_125_fgen.txt'; % --2bc
+filename_pico='20MHz_125_pico_125.txt'; % --2bc
+filename_schottky='20MHz_125_pico_125_up_up.txt'; % --2bc
+
 %% section used to generate some sinusoids in order to evaluate pico's  and pick-up performances
 
-filename_fungen='out0,511kHz.txt';
-filename_pico='511kHz.txt';
-
-fpuls=0.511*10^6; % Hz
+fpuls=20.077*10^6; % Hz --2bc
 [time,signal_m]=generatesin(fpuls,intTime,fsamp); % pass to funsin.m
 n=size(time,1); %number of samples []
 df=fsamp/n; %frequency step [Hz]
@@ -34,19 +38,34 @@ freq=(0:df:fsamp-df)'; %frequency vector [Hz]
 FFT=fft(signal_m);
 
 %% generation tool
+
 signalgenwrite(filewrite,fileread,signal_m,fsamp,gain,repeat); % generates a .ini file containing sin values
 
 %% saving matrices and plot section
-[T_sin,F_sin,t_sin,f_sin]=funsin(filename_fungen,filename_pico,intTime,fsamp,time,freq,signal_m,FFT);
+
+[T_sin,F_sin,t_sin,f_sin]=funsin(filename_fungen,filename_pico,fsamp_pico,fsamp,time,freq,signal_m,FFT);
 
 PlotTimesFreqfig(t_sin,f_sin,T_sin,F_sin);
-title('Sinusoid at fpuls = 511 kHz','FontSize',20);
+% adapt also 'title' and 'legend'
+title('Vertical Schottky response to sinusoid at fpuls = 0.511 MHz (fgen = 125 MHz, fsamp\_pico = 125 MHz)','FontSize',20);
 legend('MATLAB','fun\_gen','picoscope','FontSize',16);
+
+%% saving matrices and plot section for matlab+fungen && matlab+pico+schottky
+
+[T,F,t,f]=funsin(filename_fungen,filename_pico,fsamp_pico,fsamp,time,freq,signal_m,FFT,filename_schottky);
+PlotTimesFreqfig(t(:,[1 2]),f(:,[1 2]),T(:,[1 2]),F(:,[1 2]));
+% adapt also 'title' and 'legend'
+title('Sinusoid at fpuls = 0.511 MHz (fgen = 125 MHz, fsamp\_pico = 125 MHz)','FontSize',20);
+legend('MATLAB','fun\_gen','FontSize',16);
+% adapt also 'title' and 'legend'
+PlotTimesFreqfig(t(:,[1 3 4]),f(:,[1 3 4]),T(:,[1 3 4]),F(:,[1 3 4]));
+title('Sinusoid at fpuls = 0.511 MHz (fgen = 125 MHz, fsamp\_pico = 125 MHz)','FontSize',20);
+legend('MATLAB','pico\_only','pico\_schottky','FontSize',16);
 
 %% section used to generate gaussian pulse in order to evaluate pico's
 
 t=(-intTime/2:dt:intTime/2)';
 mu=0;
-sigma=0.00001;
+sigma=1;
 signal=exp(-(t-mu).^2/(2*sigma^2));
 signalgenwrite(filewrite,fileread,signal,fsamp,gain,repeat);
