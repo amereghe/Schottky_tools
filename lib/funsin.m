7a50f0f39feb372de39eeec0a449ed01928e79b7
mauro-di-liddo <mauro.diliddo9@gmail.com>
2022-01-20 14:09:09 +0100

    file_schottky file2var in exist control

--- lib/funsin.m
+++ lib/funsin.m
@@ -4,7 +4,7 @@ function [T,F,t,f] = funsin(file_fungen,file_pico,fsamp_pico,fsamp,time_matlab,f
 
 signal_fungen=funread(file_fungen);
 [time_pico,signal_pico]=oscread(file_pico);
-if exist(file_schottky,'file')
+if exist('file_schottky','var')
     [time_schottky,signal_schottky]=oscread(file_schottky);
 end
 n_fungen=length(signal_fungen);
@@ -18,27 +18,27 @@ freq_pico=(0:df_pico:fsamp_pico-df_pico)';
 
 T=padding(signal_matlab,signal_fungen);
 T=padding(T,signal_pico);
-if exist(file_schottky,'file')
+if exist('file_schottky','var')
     T=padding(T,signal_schottky);
 end
 sFUNGEN=fft(signal_fungen);
 sPICO=fft(signal_pico);
 F=padding(sMATLAB,sFUNGEN);
 F=padding(F,sPICO);
-if exist(file_schottky,'file')
+if exist('file_schottky','var')
     sSCHOTTKY=fft(signal_schottky);
     F=padding(F,sSCHOTTKY);
 end
 
 t=padding(time_matlab,time_fungen);
 t=padding(t,time_pico);
-if exist(file_schottky,'file')
+if exist('file_schottky','var')
     t=padding(t,time_schottky);
 end
 
 f=padding(freq_matlab,freq_fungen);
 f=padding(f,freq_pico);
-if exist(file_schottky,'file')
+if exist('file_schottky','var')
     f=padding(f,freq_pico);
 end
 
