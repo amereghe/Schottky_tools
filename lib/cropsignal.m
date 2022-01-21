409250fc7bbfad74a94c0631c22a3296c5ef3c87
mauro-di-liddo <mauro.diliddo9@gmail.com>
2022-01-21 11:24:14 +0100

    crop function fixes

--- lib/cropsignal.m
+++ lib/cropsignal.m
@@ -1,31 +1,43 @@
-function [iSigStart,iSigStop]=cropsignal(SigIn,thresh,nCons)
+function [iSigStart,iSigStop]=cropsignal(SigIn,thresh,nCons,nSigmNoise)
 %   SigIn has noise before and after useful signal!
     if ( ~exist('thresh','var') ), thresh=0.023; end % [V]
     if ( ~exist('nCons','var') ), nCons=10; end %
+    if ( ~exist('nSigmNoise','var') ), nSigmNoise=5; end %
     % find intervals with noise
-    myDiff=diff([0;abs(SigIn);0]<thresh);
+    thresh=thresh*max(abs(SigIn));
+    myDiff=diff([abs(thresh)*1.1;abs(SigIn);abs(thresh)*1.1]<thresh);
     iNoiseStart = find(myDiff==1);
     iNoiseStop = find(myDiff==-1)-1;
     nIntervals=length(iNoiseStart);
     if ( nIntervals<2 ), error("...only one noise range!"); end
-    iSigStart=zeros(nIntervals,1); iSigStop=zeros(nIntervals,1);
+    iSigStart=[]; iSigStop=[];
     % get indices of actual signal, assuming that signal is a pure sinusoid
     nSig=0;
     for jj=1:numel(iNoiseStart) % loop through each block of noise
-        if ( iNoiseStart(jj)-iNoiseStop(jj)<nCons ), continue; end   % most probably a node
-        padded=[0;SigIn(iNoiseStart(jj):iNoiseStop(jj));0];
-        if ( jj<numel(iNoiseStart) )
+        if (iNoiseStop(jj)-iNoiseStart(jj)<nCons)
+            continue;   % most probably a node
+        end
+        mySig=SigIn(iNoiseStart(jj):iNoiseStop(jj));
+        meanNoise=mean(mySig(1+round(nCons/2):end-round(nCons/2)));
+        stdNoise=std(mySig(1+round(nCons/2):end-round(nCons/2)));
+        padded=abs([0;mySig-meanNoise;0])/stdNoise;
+        myDiff=diff(padded>nSigmNoise);
+        if (jj<numel(iNoiseStart))
             nSig=nSig+1; % a new signal
             % get index where following signal actually starts
-            myDiff=diff(padded>0);
-            iStart = find(myDiff==1);
-            iSigStart(nSig)=iNoiseStop(jj)-iStart(end);
+            iSigStart(nSig)=iNoiseStop(jj);
+            iStart=find(myDiff==1);
+            if ~isempty(iStart)
+                iSigStart(nSig)=iSigStart(nSig)-(length(padded)-iStart(end));
+            end
         end
-        if ( jj>1 )
+        if (jj>1)
             % get index where previous signal actually stops
-            myDiff=diff(padded<0);
-            iStop = find(myDiff==-1)-1;
-            iSigStop(nSig)=iNoiseStart(jj)+iStop(1);
+            iSigStop(nSig)=iNoiseStart(jj)-1;
+            iStop=find(myDiff==-1)-1;
+            if ~isempty(iStop)
+                iSigStop(nSig)=iSigStop(nSig)+iStop(1);
+            end
         end
     end
 end
