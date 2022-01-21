8acd0a7a072a6d2e83acbc64c485d7854b2ab99b
mauro-di-liddo <mauro.diliddo9@gmail.com>
2022-01-21 15:02:17 +0100

    mainosc upgrade for cropping

--- lib/mainosc.m
+++ lib/mainosc.m
@@ -24,13 +24,13 @@ signalgenwrite(filewrite,fileread,signal,fsamp,gain,repeat);
 
 %% file_IDs for reading
 
-filename_fungen='20MHz_125_fgen.txt'; % --2bc
-filename_pico='20MHz_125_pico_125.txt'; % --2bc
-filename_schottky='20MHz_125_pico_125_up_down.txt'; % --2bc
+filename_fungen='511kHz_125_fgen.txt'; % --2bc
+filename_pico='511kHz_125_pico_125.txt'; % --2bc
+filename_schottky='511kHz_125_pico_125_up_up.txt'; % --2bc
 
 %% section used to generate some sinusoids in order to evaluate pico's  and pick-up performances
 
-fpuls=20.077*10^6; % Hz --2bc
+fpuls=0.511*10^6; % Hz --2bc
 [time,signal_m]=generatesin(fpuls,intTime,fsamp); % pass to funsin.m
 n=size(time,1); %number of samples []
 df=fsamp/n; %frequency step [Hz]
@@ -41,14 +41,28 @@ FFT=fft(signal_m); % -- we can use also fftshift()? different spectra, maybe cor
 
 signalgenwrite(filewrite,fileread,signal_m,fsamp,gain,repeat); % generates a .ini file containing sin values
 
-%% saving matrices and plot section (no schottky)
+%% saving matrices and plot section
 
-[T_sin,F_sin,t_sin,f_sin]=funsin(filename_fungen,filename_pico,fsamp_pico,fsamp,time,freqmat,signal_m,FFT);
-
+[T_sin,F_sin,t_sin,f_sin]=funsin(filename_fungen,filename_schottky,fsamp_pico,fsamp,time,freqmat,signal_m,FFT);
+[start,stop]=cropsignal(T_sin(:,3));
+if isempty(start)
+    start=1;
+end
+% if isempty(stop)
+%     stop=size(T_sin,1);
+% end
+T_schottky_cropped=T_sin(start:stop,3);
+F_schottky_cropped=fft(T_schottky_cropped);
+T_sin=padding(T_sin,T_schottky_cropped);
+F_sin=padding(F_sin,F_schottky_cropped);
+t_schottky_cropped=(0:dt:(stop-1)*dt)';
+t_sin=padding(t_sin,t_schottky_cropped);
+f_schottky_cropped=(0:df:fsamp-df)';
+f_sin=padding(f_sin,f_schottky_cropped);
 PlotTimesFreqfig(t_sin,f_sin,T_sin,F_sin);
 % adapt also 'title' and 'legend'
-title('Vertical Schottky response to sinusoid at fpuls = 0.511 MHz (fgen = 125 MHz, fsamp\_pico = 125 MHz)','FontSize',20);
-legend('MATLAB','fun\_gen','picoscope','FontSize',16);
+title('Vertical Schottky response to sinusoid at fpuls = 0.511 MHz (fgen = 125 MHz, fsamp\_pico = 125 MHz), up_up wideband measure','FontSize',20);
+legend('MATLAB','fun\_gen','Schottky','Schottky cropped','FontSize',16);
 
 %% saving matrices and plot section for matlab+fungen && matlab+pico+schottky
 
