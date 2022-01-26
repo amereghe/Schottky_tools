a80db85ae850e5b8c43cdd3083ecc5c92595e4a5
mauro-di-liddo <mauro.diliddo9@gmail.com>
2022-01-26 12:14:04 +0100

    added section to compare different measure modalities

--- lib/mainosc.m
+++ lib/mainosc.m
@@ -24,13 +24,14 @@ signalgenwrite(filewrite,fileread,signal,fsamp,gain,repeat);
 
 %% file_IDs for reading
 
-filename_fungen='20MHz_125_fgen.txt'; % --2bc
-filename_pico='20MHz_125_pico_125.txt'; % --2bc
-filename_schottky='20MHz_125_pico_125_up_down_lres.txt'; % --2bc
+filename_fungen='511kHz_125_fgen.txt'; % --2bc
+filename_pico='511kHz_125_pico_125.txt'; % --2bc
+filename_schottky='511kHz_125_pico_125_up_up_hres.txt'; % --2bc
 
 %% section used to generate some sinusoids in order to evaluate pico's  and pick-up performances
 
-fpuls=20.077*10^6; % Hz --2bc
+fpuls=[0.511,2.179,20.077]*10^6; % Hz --2bc
+% fpuls=[0.468997,1.343040,1.756816,1.945041,2.209601,2.290283,2.753049,5.290283,7.543041,10.140529,12.577011,15.175314,17.577090,20.077017]*10^6;
 [time,signal_m]=generatesin(fpuls,intTime,fsamp); % pass to funsin.m
 n=size(time,1); %number of samples []
 df=fsamp/n; %frequency step [Hz]
@@ -41,12 +42,12 @@ FFT=fft(signal_m); % -- we can use also fftshift()? different spectra, maybe cor
 
 signalgenwrite(filewrite,fileread,signal_m,fsamp,gain,repeat); % generates a .ini file containing sin values
 
-%% saving matrices and plot section (cropped)
+%% saving matrices and plot section (cropped&&normalized)
 
 [T_sin,F_sin,t_sin,f_sin]=funsin(filename_fungen,filename_pico,fsamp_pico,fsamp,time,freqmat,signal_m,FFT,filename_schottky);
 % here you can also add filename_schottky instead of filename_pico or as
 % last input, but T_schottky_cropped should pass from (start:stop,3->4)
-[start,stop]=cropsignal(T_sin(:,4),0.075);
+[start,stop]=cropsignal(T_sin(:,4)); % add other inputs
 % if isempty(start)
 %     start=1;
 % end
@@ -72,13 +73,51 @@ f_schottky_cropped=(0:df_schottky:fsamp-df)';
 f_sin=padding(f_sin,f_schottky_cropped);
 [T_sin(:,1),F_sin(:,1)]=normalize(T_sin(:,1),F_sin(:,1),df);
 [T_sin(:,5),F_sin(:,5)]=normalize(T_sin(:,5),F_sin(:,5),df_pico);
-[T_sin(:,6),F_sin(:,6)]=normalize(T_sin(:,6),F_sin(:,6),df_schottky);
+[T_sin(:,6),F_sin(:,6),t_max,f_max]=normalize(T_sin(:,6),F_sin(:,6),df_schottky);
 PlotTimesFreqfig(t_sin(:,[1 5 6]),f_sin(:,[1 5 6]),T_sin(:,[1 5 6]),F_sin(:,[1 5 6]));
 % adapt also 'title' and 'legend'
 title('Vertical Schottky response to sinusoid at fpuls = 20.077 MHz (fgen = 125 MHz, fsamp\_pico = 125 MHz), up\_down low res measure','FontSize',20);
-% title('Sinusoids at fpuls = [0.511, 2.179, 20.077] MHz (fgen = 125 MHz, fsamp\_pico = 125 MHz), up\_down wideband measure','FontSize',20);
-legend('MATLAB\_norm','Pico\_only\_norm','Schottky cropped\_norm','FontSize',16); % 1->matlab, 3->pico_only, 5->schottky cropped
+% title('Sinusoids at fpuls = [0.511, 2.179, 20.077] MHz (fgen = 125 MHz, fsamp\_pico = 125 MHz), up\_down low res measure','FontSize',20);
+legend('MATLAB\_norm','Pico\_only\_norm','Schottky\_cropped\_norm','FontSize',16); % 1->matlab, 3->pico_only, 5->schottky cropped
 % FWHM=df*fwhm(f_sin(:,4),F_sin(:,4)); % --2bcontrolled and c
+
+%% evaluate differences btw single fft and all fft
+% signals without zeros between them
+fpuls=[0.511,2.179,20.077]*10^6; % Hz --2bc
+[time,signal_m]=generatesin(fpuls,intTime,fsamp); % pass to funsin.m
+n=size(time,1); %number of samples []
+df=fsamp/n; %frequency step [Hz]
+freqmat=(0:df:fsamp-df)'; %frequency vector [Hz]
+FFT=fft(signal_m); % -- we can use also fftshift()? different spectra, maybe correct
+% single signals
+[t1,signal1]=generatesin(fpuls(1),intTime,fsamp);
+n1=size(t1,1);
+df1=fsamp/n1;
+freq1=(0:df1:fsamp-df1)';
+FFT1=fft(signal1);
+[t2,signal2]=generatesin(fpuls(2),intTime,fsamp);
+n2=size(t2,1);
+df2=fsamp/n2;
+freq2=(0:df2:fsamp-df2)';
+FFT2=fft(signal2);
+[t3,signal3]=generatesin(fpuls(3),intTime,fsamp);
+n3=size(t3,1);
+df3=fsamp/n3;
+freq3=(0:df3:fsamp-df3)';
+FFT3=fft(signal3);
+tt=padding(time,t1);
+tt=padding(tt,t2);
+tt=padding(tt,t3);
+ff=padding(freqmat,freq1);
+ff=padding(ff,freq2);
+ff=padding(ff,freq3);
+sig=padding(signal_m,signal1);
+sig=padding(sig,signal2);
+sig=padding(sig,signal3);
+FFT_all=padding(FFT,FFT1);
+FFT_all=padding(FFT_all,FFT2);
+FFT_all=padding(FFT_all,FFT3);
+PlotTimesFreqfig(tt,ff,sig,FFT_all);
 
 %% +norm (to evaluate possible reflection of signal coming from up_down || down_up)
 
@@ -97,6 +136,73 @@ PlotTimesFreqfig(t_ss,f_ss,T_ss,F_ss);
 % adapt also 'title' and 'legend'
 title('Vertical Schottky response to sinusoid at fpuls = 20.077 MHz (fgen = 125 MHz, fsamp\_pico = 125 MHz), up\_down wideband measure','FontSize',20);
 legend('MATLAB','fun\_gen','Schottky','Schottky cropped','FontSize',16);
+
+%% plot only_pico && only Schottky (carefully match filenames with fpuls)
+
+fpuls=[0.511,2.179,20.077]*10^6; % Hz --2bc
+% single signals
+[t1,signal1]=generatesin(fpuls(1),intTime,fsamp);
+n1=size(t1,1);
+df1=fsamp/n1;
+freq1=(0:df1:fsamp-df1)';
+FFT1=fft(signal1);
+[t2,signal2]=generatesin(fpuls(2),intTime,fsamp);
+n2=size(t2,1);
+df2=fsamp/n2;
+freq2=(0:df2:fsamp-df2)';
+FFT2=fft(signal2);
+[t3,signal3]=generatesin(fpuls(3),intTime,fsamp);
+n3=size(t3,1);
+df3=fsamp/n3;
+freq3=(0:df3:fsamp-df3)';
+FFT3=fft(signal3);
+
+filename_fungen_1='511kHz_125_fgen.txt';
+filename_fungen_2='2MHz_125_fgen.txt';
+filename_fungen_3='20MHz_125_fgen.txt';
+filename_pico_1='511kHz_125_pico_125.txt';
+filename_pico_2='2MHz_125_pico_125.txt';
+filename_pico_3='20MHz_125_pico_125.txt';
+% select the modality and operation features
+filename_schottky_1='511kHz_125_pico_125_up_up.txt';
+filename_schottky_2='2MHz_125_pico_125_up_up.txt';
+filename_schottky_3='20MHz_125_pico_125_up_up.txt';
+% you can easily set up filename_schottky for the same frequency in
+% different measure modalities: just use a single frequency so a single
+% *_fungen and a single *_pico_125 file and modify also the legend related
+% to the second plot (plot wideband, low res, high res)
+
+[T_pico_schottky1,F_pico_schottky1,t_pico_schottky1,f_pico_schottky1]=funsin(filename_fungen_1,filename_pico_1,fsamp_pico,fsamp,t1,freq1,signal1,FFT1,filename_schottky_1);
+[T_pico_schottky2,F_pico_schottky2,t_pico_schottky2,f_pico_schottky2]=funsin(filename_fungen_2,filename_pico_2,fsamp_pico,fsamp,t2,freq2,signal2,FFT2,filename_schottky_2);
+[T_pico_schottky3,F_pico_schottky3,t_pico_schottky3,f_pico_schottky3]=funsin(filename_fungen_3,filename_pico_3,fsamp_pico,fsamp,t3,freq3,signal3,FFT3,filename_schottky_3);
+
+pico_t=[t_pico_schottky1(:,3),t_pico_schottky2(:,3)];
+pico_t=padding(pico_t,t_pico_schottky3(:,3));
+pico_T=[T_pico_schottky1(:,3),T_pico_schottky2(:,3)];
+pico_T=padding(pico_T,T_pico_schottky3(:,3));
+pico_f=[f_pico_schottky1(:,3),f_pico_schottky2(:,3)];
+pico_f=padding(pico_f,f_pico_schottky3(:,3));
+pico_F=[F_pico_schottky1(:,3),F_pico_schottky2(:,3)];
+pico_F=padding(pico_F,F_pico_schottky3(:,3));
+
+schottky_t=[t_pico_schottky1(:,4),t_pico_schottky2(:,4)];
+schottky_t=padding(pico_t,t_pico_schottky3(:,4));
+schottky_T=[T_pico_schottky1(:,4),T_pico_schottky2(:,4)];
+schottky_T=padding(pico_T,T_pico_schottky3(:,4));
+schottky_f=[f_pico_schottky1(:,4),f_pico_schottky2(:,4)];
+schottky_f=padding(pico_f,f_pico_schottky3(:,4));
+schottky_F=[F_pico_schottky1(:,4),F_pico_schottky2(:,4)];
+schottky_F=padding(pico_F,F_pico_schottky3(:,4));
+% pico plot
+PlotTimesFreqfig(pico_t,pico_f,pico_T,pico_F);
+% adapt also 'title' and 'legend'
+title('Pico signals at fpuls = [0.511,2.179,20.077] MHz (fgen = 125 MHz, fsamp\_pico = 125 MHz), up\_up wideband measure','FontSize',20);
+legend('fpuls(1)','fpuls(2)','fpuls(3)','FontSize',16);
+% schottky plot
+PlotTimesFreqfig(schottky_t,schottky_f,schottky_T,schottky_F);
+% adapt also 'title' and 'legend'
+title('Schottky signals at fpuls = [0.511,2.179,20.077] MHz (fgen = 125 MHz, fsamp\_pico = 125 MHz), up\_up wideband measure','FontSize',20);
+legend('fpuls(1)','fpuls(2)','fpuls(3)','FontSize',16);
 
 %% saving matrices and plot section for matlab+fungen && matlab+pico+schottky
 
@@ -123,9 +229,9 @@ title('Ratio between ffts','FontSize',20);
 legend('pico/MATLAB','schottky/MATLAB','FontSize',16);
 
 %% section used to generate gaussian pulse in order to evaluate pico's
-
-t=(-intTime/2:dt:intTime/2)';
+intTime=intTime/3.5; % having declared intTime=200us in first rows of main
+t=(-intTime/2:dt:intTime/2-dt)';
 mu=0;
-sigma=1;
+sigma=10^-5;
 signal=exp(-(t-mu).^2/(2*sigma^2));
-signalgenwrite(filewrite,fileread,signal,fsamp,gain,repeat);
+% signalgenwrite(filewrite,fileread,signal,fsamp,gain,repeat);
