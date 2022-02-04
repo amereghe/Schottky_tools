99b27da4945aadbcd081ac1509fbb6e391b0845a
mauro-di-liddo <mauro.diliddo9@gmail.com>
2022-02-04 23:56:47 +0100

    oscread for 2chs acquisition

--- lib/oscread.m
+++ lib/oscread.m
@@ -1,22 +1,26 @@
-function [time,signal] = oscread(filename)
+function [time,signal] = oscread(filename,ch)
 
 %OSCREAD used to save data coming from the picoscope in a txt file
 %   There are two columns in the file: the first column is related to the
 %   temporal samples instants while the second column stores the respective
 %   values of the signal at each time (row).
 
-% if(~exist(filename,'file'))
-%         filename='20MHz_125_pico_up_down_lres.txt';
-% end
-
 file=fopen(filename,'r');
-h=textscan(file,'%s %s','headerlines',1);
+h=textscan(file,'%s%s','headerlines',1);
 fclose(file);
 H=h{1,2}{1,1};
-file=fopen(filename,'r');
-a=textscan(file,'%n	%n','headerlines',3); % % two rows and undefined columns as we do noot know f_sample a priori
-fclose(file);
-A=cell2mat(a);
+
+if(~exist('ch','var'))
+        file=fileopen(filename,'r');
+        a=textscan(file,'%n %n','headerlines',3); % % two rows and undefined columns as we do noot know f_sample a priori
+        fclose(file);
+        A=cell2mat(a);
+else
+    file=fopen(filename,'r');
+    a=textscan(file,'%n	%n %n','headerlines',3); % % three rows and undefined columns as we do noot know f_sample a priori
+    fclose(file);
+    A=cell2mat(a);
+end
 
 
 str='(mV)';
@@ -24,8 +28,16 @@ str='(mV)';
 time=10^-6*A(:,1);
 if strcmp(H,str)
     signal=10^-3*A(:,2);
+    if(exist('ch','var'))
+        tmp=10^-3*A(:,3);
+        signal=[signal,tmp];
+    end
 else
     signal=A(:,2);
+    if(exist('ch','var'))
+        tmp=A(:,3);
+        signal=[signal,tmp];
+    end
 end
 
 end
