e7d6be4637603a2d4cb0b864bb3f4478d71d43f8
Mereghetti Alessio <Alessio.Mereghetti@Main.Cnao.group>
2022-05-03 12:33:13 +0200

    actual modifications to function for simulating particle passages at the Schottky

--- lib/SimulatePartPassages.m
+++ lib/SimulatePartPassages.m
@@ -1,100 +1,98 @@
-function [xx] = generate(fs,tt,qq,friv,ww,taus,a0,aa)
-% generate             actually generates the time signal
+function yy = SimulatePartPassages(tt,friv,fs,taus,qq,a0,aa,sigType,as,ws)
+% SimulatePartPassages             actually generates the time signal of 
+%                                     particles passing at the Schottky,
+%                                     turn by turn
 %
-% The signal is generated as the repetition of a basic signal, representing a
-%    single of a single particle at the Schottky monitor.
+% The signal is generated as the repetition of a basic signal, given by a
+%    single passage of a single particle at the Schottky monitor.
 % Signals that can be generated are:
 % - sigma/longitudinal: the sum of the signal recorded by both plates of
-%   the Schottky pick-up (same plane). This signal is proportional to
-%   particle charge (i.e. beam intensity);
+%   the Schottky pick-up. This signal gives insights into the longitudinal
+%   beam dynamics and it is proportional to particle charge (i.e. beam intensity);
 % - delta/transverse: the difference of the signal recorded by both plates of
-%   the Schottky pick-up (same plane). This signal is proportional to
-%   particle position;
+%   the Schottky pick-up (same plane). This signal gives insights into the
+%   transverse beam dynamics and it is proportional to particle position;
 %
 % input:
-% - fs (scalar, double precision): synchrotron frequency [Hz];
 % - tt (1D array, double precision): time array [s];
-% - qq (scalar, double precision): betatron tune (int+frac) [];
 % - friv (scalar, double precision): revolution frequency [Hz];
-% - ww (scalar, double precision): time length of the single-particle signal [s];
+% - fs (scalar, double precision): synchrotron frequency [Hz];
 % - taus (scalar, double precision): amplitude of synchrotron motion [s];
+% - qq (scalar, double precision): betatron tune (int+frac) [];
 % - a0 (scalar, double precision): average value of betatron modulation [].
 %   This parameter can mimic a closed-orbit offset;
-% - aa (scalar, double precision): amplitude of betatron modulation [].
+% - aa (scalar, double precision): amplitude of betatron modulation [];
+% - sigType (string): type of signal (DELTA/RECT/GAUSS);
+% - as (scalar, double precision): amplitude of signal [V] (all signals);
+% - ws (scalar, double precision): width of signal [s] (only RECT and GAUSS);
+%   RECT: full duration of RECT;
+%   GAUSS: sigma of Gaussian profile;
 %
 % output:
-% - xx (1D array, double precision): time signal [s]. It is of the same
+% - yy (1D array, double precision): time signal [s]. It is of the same
 %   size as tt;
 
     %% parameters
-    nn=size(tt,1);                % number of samples []
-    dt=tt(2)-tt(1);               % time resolution [s]
-    intTime=tt(end)+dt;           % integration time [s]
-    nCentres=round(intTime*friv); % number of full signals []
-    tau=((0:nCentres)/friv)';     % central time of particle passage [s]
+    nSamples=size(tt,1);            % number of samples []
+    dt=tt(2)-tt(1);                 % time resolution [s]
+    intTime=tt(end)-tt(1);          % integration time [s]
+    
+    % - initialisation
+    yy=zeros(nSamples,1);
+
+    %% integrity checks
+    if ( CompFloats((nSamples-1)*dt,intTime,"GT") )
+        error("...something wrong with the signal generation!");
+    end
+    
+    if ( as<=0 ), warning("...as<=0: null signal!!"); end
+    
+    % signal types
+    if ( ~strcmpi(extractBetween(sigType,1,5),"DELTA") )
+        if ( ws<0 ), error("...negative duration of single passage signal!"); end
+        if ( ws==0 )
+            warning("...reverting signal to DELTA!"); sigType="DELTA";
+        end
+        if ( ~strcmpi(extractBetween(sigType,1,4),"RECT") & ~strcmpi(extractBetween(sigType,1,5),"GAUSS") )
+            error("...un-recognised signal type: %s (only DELTA, RECT, GAUSS)",sigType);
+        end
+    end
+    
+    % longitudinal beam dynamics
+    if (ismissing(fs)), fs=0; end
+    if (ismissing(taus)), taus=0; end
+    if ( (fs==0 && taus~=0) | (fs~=0 && taus==0) )
+        warning("...inconsisten set of values of fs and taus: setting to 0.0 both!");
+        fs=0; taus=0;
+    end
+    % transverse beam dynamics
+    if (ismissing(qq)), qq=0; end
+    if (ismissing(aa)), aa=0; end
+    if ( (qq==0 && aa~=0) | (qq~=0 && aa==0) )
+        warning("...inconsisten set of values of qq and aa: setting to 0.0 both!");
+        qq=0; aa=0;
+    end
 
     %% atual signal
-    % - initialisation
-    xx=zeros(nn,1);
+    % - number of passages []
+    nCentres=floor(intTime*friv);
+    
+    % - central times
+    tau=(((1:nCentres)-0.5)/friv+tt(1))'; % central time of particle passage [s]
 
     % - synchrotron motion (changes time of passage of particle turn by turn)
     if(fs~=0), tau=tau+taus*sin(2*pi*fs*tau); end
     
-    % - central time
-    add=0; if (tau(end)/dt<nn), add=1; end
-    for kk=1:nCentres+add
-        % xx=InsertSingle(tau(kk),ww,dt,tt,xx); %longitudinal bunched, maybe useful for gauss
-        ind=round(tau(kk)/dt)+1;
-        xx(ind)=1;
+    % - actual signals times
+    if ( strcmpi(extractBetween(sigType,1,5),"DELTA") )
+        yy=GenerateSingleDelta(tt,yy,tau,as);
+    elseif ( strcmpi(extractBetween(sigType,1,4),"RECT") )
+        yy=GenerateSingleRect(tt,yy,tau,as,ws);
+    elseif ( strcmpi(extractBetween(sigType,1,5),"GAUSS") )
+        yy=GenerateSingleGauss(tt,yy,tau,as,ws);
     end
 
     % - betatron motion (i.e. amplitude modulation, like transverse position)
-    if(qq~=0), xx=xx.*(a0+aa*sin(2*pi*qq*friv*tt)); end
+    if(qq~=0), yy=yy.*(a0+aa*sin(2*pi*qq*friv*tt)); end
 
-end
-
-function [SS] = InsertSingle(t0,ww,dt,tS,SS)
-% InsertSingle        inserts a single-particle,single-passage signal at t0
-%
-% input:
-% - tS, SS (1D arrays, double precision): time [s] and original signal [];
-% - t0 (scalar, double precision): central time at which the
-%   single-particle, single-passage signal should be inserted [s];
-% - ww (scalar, double precision): width of the single-particle, 
-%   single-passage signal (non-zero part) should be inserted [s];
-% - dt (scalar, double precision): resolution of time domain [s];
-%
-% output:
-% - modified SS array;
-
-    % basic single-particle, single passage signal to insert
-    hRange=2.5;
-    tt=(t0-hRange*ww:dt:t0+hRange*ww);
-    ss=MyRect(tt-t0,ww);
-
-    % places in S to be modified
-    t1=min(tt)-dt/2; t2=max(tt)+dt/2;
-    indT=find(t1<=tS & tS<=t2);
-
-    % actually insert signal
-    if ( length(indT)==length(tt) )
-        SS(indT)=ss;
-    end
-
-end
-
-function ff=MyRect(tt,ww)
-% MyRect           generates a rect signal
-% 
-% input:
-% - tt (1D array, double precision): time [s];
-% - ww (scalar, double precision): width of the rect [s];
-%
-% output:
-% - ff (1D array, double precision): rect signal []. The array has the
-%      same size as that of tt;
-%
-    eps=1E-22;
-    ff=zeros(length(tt),1);
-    ff(abs(tt)<abs(ww)+eps)=1;
 end
