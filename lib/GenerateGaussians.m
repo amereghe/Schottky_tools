d1970600244541a31b7a7f7058f2e679de7b4bb9
Mereghetti Alessio <Alessio.Mereghetti@Main.Cnao.group>
2022-05-12 15:31:11 +0200

    function generating gaussians for particle simulation now takes a tOn parameter and no longer nSigma

--- lib/GenerateGaussians.m
+++ lib/GenerateGaussians.m
@@ -1,8 +1,8 @@
-function yy=GenerateGaussians(tt,yy,t0,AA,sigT,nSig)
-    if (~exist('nSig','var')), nSig=5; end
+function yy=GenerateGaussians(tt,yy,t0,AA,sigT,tOn)
     for ii=1:length(sigT)
+        if (~exist('tOn','var')), nSig=5; tOn=nSig*sigT(ii); end
         for jj=1:length(t0)
-            myIndices=(t0(jj)-nSig*sigT(ii)<=tt & tt<=t0(jj)+nSig*sigT(ii));
+            myIndices=(t0(jj)-tOn<=tt & tt<=t0(jj)+tOn);
             yy(myIndices,ii)=yy(myIndices,ii)+AA(ii)/(sqrt(2*pi)*sigT(ii))*exp(-0.5*((tt(myIndices)-t0(jj))/sigT(ii)).^2);
         end
     end
