8b6145e0f73695d391c0ad51b8fe264d75f865d8
Mereghetti Alessio <Alessio.Mereghetti@Main.Cnao.group>
2022-05-13 15:35:31 +0200

    GenerateGaussians now updates first value of signal in current time range only if it is the starting one

--- lib/GenerateGaussians.m
+++ lib/GenerateGaussians.m
@@ -2,8 +2,14 @@ function yy=GenerateGaussians(tt,yy,t0,AA,sigT,tOn)
     for ii=1:length(sigT)
         if (~exist('tOn','var')), nSig=5; tOn=nSig*sigT(ii); end
         for jj=1:length(t0)
-            myIndices=(t0(jj)-tOn<=tt & tt<=t0(jj)+tOn);
-            yy(myIndices,ii)=yy(myIndices,ii)+AA(ii)/(sqrt(2*pi)*sigT(ii))*exp(-0.5*((tt(myIndices)-t0(jj))/sigT(ii)).^2);
+            myIndices=(t0(jj)-tOn<tt & tt<=t0(jj)+tOn);
+            % update signal array only in case of non-zero indices
+            if ( sum(myIndices)>0 )
+                % update value at lower extreme only in case of first
+                %    Gaussian
+                if ( tt(1)==t0(jj)-tOn ), myIndices(1)=true; end
+                yy(myIndices,ii)=yy(myIndices,ii)+AA(ii)/(sqrt(2*pi)*sigT(ii))*exp(-0.5*((tt(myIndices)-t0(jj))/sigT(ii)).^2);
+            end
         end
     end
 end
