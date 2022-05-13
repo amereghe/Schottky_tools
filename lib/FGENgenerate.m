07235ea8210471d209e98677da0d157db471747e
Mereghetti Alessio <Alessio.Mereghetti@Main.Cnao.group>
2022-05-13 15:33:47 +0200

    FGENgenerate can also handle GAUSS trains, not only sinusoids

--- lib/FGENgenerate.m
+++ lib/FGENgenerate.m
@@ -1,13 +1,15 @@
-function [tOut,sOut,nSamps] = FGENgenerateSinusoids(fPuls,intTime,fSamp,lConcatenate)
-% FGENgenerateSinusoids     generates some sinusoids with full periods, such
-%                             that each sinusoid can be repeated indefinitively
+function [tOut,sOut,nSamps] = FGENgenerate(fPuls,intTime,fSamp,lConcatenate,sigType,as,ws)
+% FGENgenerate     generates some signals with full periods, such
+%                        that each sinusoid can be repeated indefinitively
 
     %% parameters
-    if (~exist('lConcatenate','var')), lConcatenate=true; end
     dt=1/fSamp;                            % time resolution [s]
     basicTime=(0:dt:intTime)';
     nSamples=size(basicTime,1);            % number of samples []
     nPulseFreqs=length(fPuls);
+    if (~exist('lConcatenate','var')), lConcatenate=true; end
+    if (~exist('sigType','var')), sigType="SIN"; end
+    if (~exist('as','var')), as=ones(nPulseFreqs,1); end
     
     %% output variables
     if ( lConcatenate )
@@ -34,7 +36,14 @@ function [tOut,sOut,nSamps] = FGENgenerateSinusoids(fPuls,intTime,fSamp,lConcate
         
         % sample signal
         tt=basicTime(iStart:nSamps(ii)+iStart-1);
-        sig=sin(2*pi*fPuls(ii)*tt);
+        if ( strcmpi(extractBetween(sigType,1,3),"SIN") )
+            sig=as(ii)*sin(2*pi*fPuls(ii)*tt);
+        elseif ( strcmpi(extractBetween(sigType,1,5),"GAUSS") )
+            nCentres=floor(tt(end)*fPuls(ii));
+            tau=(((1:nCentres)-0.5)/fPuls(ii)+tt(1))'; % central time of particle passage [s]
+            sig=zeros(length(tt),1);
+            sig=GenerateGaussians(tt,sig,tau,as(ii),ws(ii),0.5/fPuls(ii));
+        end
         
         % store data
         if ( lConcatenate )
@@ -46,6 +55,12 @@ function [tOut,sOut,nSamps] = FGENgenerateSinusoids(fPuls,intTime,fSamp,lConcate
             tOut(1:nSamps(ii),ii)=tt;
             sOut(1:nSamps(ii),ii)=sig;
         end
+    end
+    
+    if ( lConcatenate )
+        indices=(isnan(tOut));
+        tOut=tOut(~indices);
+        sOut=sOut(~indices);
     end
 
 end
