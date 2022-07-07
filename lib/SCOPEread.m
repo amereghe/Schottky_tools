b813534587f462bef378dc3107d21496e23a434a
Mereghetti Alessio <Alessio.Mereghetti@Main.Cnao.group>
2022-07-07 11:32:13 +0200

    going multi-signal with parsing of PICOscope and RFscope files

--- lib/SCOPEread.m
+++ lib/SCOPEread.m
@@ -1,4 +1,70 @@
-function [XX,YY] = SCOPEread(fileName,lType)
+function [times,signals] = SCOPEread(fileNames,lType,times,signals)
+% SCOPEread             wrapper for the actual SCOPEread function. The wrapper
+%                        simply loops over a list of files and saves data
+%                        in colums
+
+    %% input checks
+    if (~exist('lType','var')), lType="SIG"; end
+    if ( exist("times","var") & ~exist("signals","var") )
+        error("...you have provided a times array but not a signal array!");
+    end
+    if ( exist("signals","var") )
+        nSignals=size(signals,2);
+    else
+        signals=missing(); nSignals=0;
+    end
+    if ( exist("times","var") )
+        nTimes=size(times,2);
+    else
+        times=missing(); nTimes=0;
+    end
+    if ( nTimes~=nSignals )
+        error("...inconsistent set of (already existing) signals: nTimes=%d, nSignals=%d",nTimes,nSignals);
+    end
+    
+    %% actually parse files
+    nParsedFiles=0;
+    nParsedSignals=0;
+    for iFile=1:length(fileNames)
+        [tmpTime,tmpSignals]=SCOPEreadActual(fileNames(iFile),lType);
+        nTmpSignals=size(tmpSignals,2);
+        if ( nTmpSignals==0 )
+            warning("...no signal found in file!");
+            continue
+        end
+        if ( nSignals==0 )
+            % first signal(s) to be acquired
+            times=tmpTime;
+            signals=tmpSignals;
+        else
+            nVals=size(times,1); nCurrVals=size(tmpTime,1);
+            if ( nVals<nCurrVals )
+                % fill in existing columns with NaNs instead of 0.0
+                times(nVals+1:nCurrVals,:)=NaN();
+                signals(nVals+1:nCurrVals,:)=NaN();
+            end
+            for ii=1:nTmpSignals
+                % only a time column per picofile
+                times(1:nCurrVals,nSignals+ii)=tmpTime;
+            end
+            signals(1:nCurrVals,nSignals+1:nSignals+nTmpSignals)=tmpSignals;
+            if ( nVals>nCurrVals )
+                % fill in added columns with NaNs instead of 0.0
+                times(nCurrVals+1:nVals,nSignals+nTmpSignals)=NaN();
+                signals(nCurrVals+1:nVals,nSignals+nTmpSignals)=NaN();
+            end
+        end
+        nParsedFiles=nParsedFiles+1;
+        nParsedSignals=nParsedSignals+nTmpSignals;
+        nSignals=nSignals+nTmpSignals;
+    end
+    
+    %% output a summary
+    fprintf("...acquired %d files, for a total of %d signals;\n",nParsedFiles,nParsedSignals);
+
+end
+
+function [XX,YY] = SCOPEreadActual(fileName,lType)
 % SCOPEread             read data acquired with the RF SCOPE and saved in
 %                          plain ASCII file (csv)
 
