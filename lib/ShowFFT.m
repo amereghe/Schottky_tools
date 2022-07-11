d71ce4a243803bc5be06367eb591c904ff6febfd
Mereghetti Alessio <Alessio.Mereghetti@Main.Cnao.group>
2022-07-11 11:21:39 +0200

    when normalising FFTs plots, the operation is an actual shift, not a ratio

--- lib/ShowFFT.m
+++ lib/ShowFFT.m
@@ -1,8 +1,9 @@
 function ShowFFT(freq,FFTs,F0s)
     if ( exist("F0s","var") )
         xLab="f []";
+        if (size(F0s,1)==1), F0s(2,:)=0; end
     else
-        F0s=1;
+        F0s=ones(2,1); F0s(2,:)=0;
         xLab="f [Hz]";
     end
     nFFTs=size(FFTs,2);
@@ -11,8 +12,8 @@ function ShowFFT(freq,FFTs,F0s)
         if ( iFFT>1 ), hold on; end
         % indices=~isnan(FFTs(:,iFFT));
         if ( size(freq,2)==1 ), iFreq=1; else iFreq=iFFT; end
-        if ( length(F0s)==1 ), iF0=1; else iF0=iFFT; end
-        plot(freq(:,iFreq)/F0s(iF0),FFTs(:,iFFT),".-");
+        if ( size(F0s,2)==1 ), iF0=1; else iF0=iFFT; end
+        plot(freq(:,iFreq)/F0s(1,iF0),FFTs(:,iFFT)-F0s(2,iF0),".-");
     end
     grid(); xlabel(xLab); ylabel("FFT [dBm]");
     xl=xlim();
