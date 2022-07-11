a773214abc70fe91689750e1c9e269f10876ba22
Mereghetti Alessio <Alessio.Mereghetti@Main.Cnao.group>
2022-07-11 14:55:50 +0200

    dBme uses mW, not mV

--- lib/dBme.m
+++ lib/dBme.m
@@ -3,6 +3,6 @@ function yOut=dBme(yIn)
     for iSig=1:size(yIn,2)
         tmpSig=yIn(~isnan(yIn(:,iSig)),iSig);
         nPoints=length(tmpSig);
-        yOut(1:nPoints,iSig)=10*log10((abs(tmpSig)/nPoints).^2./50*1E3);
+        yOut(1:nPoints,iSig)=20*log10((abs(tmpSig)/nPoints).^2./50*1E3);
     end
 end
