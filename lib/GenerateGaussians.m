f5fa7d23f888631a4f8e33909a3ea1e47ec07ddf
Mereghetti Alessio <Alessio.Mereghetti@Main.Cnao.group>
2022-07-11 12:44:48 +0200

    reverting time cutting of basic Gaussian to 5 sig

--- lib/GenerateGaussians.m
+++ lib/GenerateGaussians.m
@@ -3,7 +3,7 @@ function yy=GenerateGaussians(tt,yy,t0,AA,sigT,AM,trunc,tOn)
     if ( ~exist('trunc','var') ), trunc=0.0; end
     if ( trunc<0 || trunc>1 ), error("truncation out of range: [0:1]!"); end
     for ii=1:length(sigT)
-        if (~exist('tOn','var')), nSig=3; tOn=nSig*sigT(ii); end
+        if (~exist('tOn','var')), nSig=5; tOn=nSig*sigT(ii); end
         for jj=1:length(t0)
             myIndices=(t0(jj)-tOn<tt & tt<=t0(jj)+tOn);
             % update signal array only in case of non-zero indices
